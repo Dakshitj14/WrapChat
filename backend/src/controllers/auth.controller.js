8f780a444a7d5b173efab4f619791a6eb7d65559
Dakshit Jha <jhadakshit2004@gmail.com>
2025-02-06 19:59:18 +0530

    backend setup done

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+    const { fullName, email, password } = req.body;
+    try{
+        
+    }
+};
+export const login = (req, res) => {
+    res.send("login controller");
+};
+export const logout = (req, res) => {
+    res.send("logout controller");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 // const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+// connectDB();
+dotenv.config();
 import authroute from './routes/auth.route.js';
 
+app.use(express.json());
 const app = express();
+const PORT = process.env.PORT || 5001;
 app.use('/api/auth', authroute);
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log('Server is running on PORT:' + PORT);
+    connectDB();
 });
