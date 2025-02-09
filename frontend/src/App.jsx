8df1166b6a81a63477ddd2bdf77254dbd4d6e490
Dakshit Jha <jhadakshit2004@gmail.com>
2025-02-09 13:47:32 +0530

    fixed default exporting of pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import NavBar from './components/NavBar.jsx';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';  
+import SettingsPage from './pages/SettingsPage.jsx';
+
 const App = () => {
   return (
-    <div className="text-red-500">
-      <h1>Hello World</h1>
+
+    <div>
+
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+
     </div>
   );
 };
